58458445804ecbf0ad13280b6ffa7959312b6dfa
mujahidur436 <indonesiaserver436436@gmail.com>
2026-06-06 23:20:26 +0600

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
-console.log("QuickTaka Loaded");
+let tg = window.Telegram.WebApp;
+
+tg.expand();
+
+if(tg.initDataUnsafe.user){
+
+    let user = tg.initDataUnsafe.user;
+
+    console.log("User ID:", user.id);
+    console.log("Name:", user.first_name);
+
+}
