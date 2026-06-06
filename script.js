dc784cc788e5a13cbf5e6e06f27ea19cad587fed
mujahidur436 <indonesiaserver436436@gmail.com>
2026-06-07 00:23:12 +0600

    Update script.js

--- script.js
+++ script.js
@@ -1,51 +1,70 @@
+const SUPABASE_URL =
+"https://gwrplydmjbdkheecnoqv.supabase.co";
+
+const SUPABASE_KEY =
+"eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imd3cnBseWRtamJka2hlZWNub3F2Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODA3NjA5MTcsImV4cCI6MjA5NjMzNjkxN30.dDFnqTHSDpt1pXSM-BU504jaB-OjLQAMmlMGsKnZVzo";
+
+const supabaseClient =
+supabase.createClient(
+SUPABASE_URL,
+SUPABASE_KEY
+);
+
 let tg = window.Telegram.WebApp;
 
 tg.expand();
 
-if(tg.initDataUnsafe.user){
+async function loadUser(){
 
-let user = tg.initDataUnsafe.user;
+if(!tg.initDataUnsafe.user){
+return;
+}
 
-let username = document.getElementById("username");
+const user =
+tg.initDataUnsafe.user;
+
+const userId = user.id;
+
+const username =
+document.getElementById("username");
 
 if(username){
 username.innerHTML =
 "👋 Welcome " + user.first_name;
 }
 
+let { data } =
+await supabaseClient
+.from("users")
+.select("*")
+.eq("id", userId)
+.single();
+
+if(!data){
+
+await supabaseClient
+.from("users")
+.insert({
+id: userId,
+first_name: user.first_name
+});
+
+data = {
+balance: 0
+};
+
 }
 
-if(localStorage.getItem("balance") == null){
-localStorage.setItem("balance","0");
-}
-
-function updateBalance(){
-
-let balance =
-localStorage.getItem("balance");
-
-let balanceElement =
+const balanceElement =
 document.getElementById("balance");
 
 if(balanceElement){
+
 balanceElement.innerHTML =
-"৳" + balance;
-}
+"৳" + Number(data.balance).toFixed(2);
 
 }
 
-updateBalance();
-let taskCount =
-localStorage.getItem("tasks") || 0;
-
-let taskElement =
-document.getElementById(
-"taskCount"
-);
-
-if(taskElement){
-
-taskElement.innerHTML =
-taskCount;
-
 }
+
+loadUser();
